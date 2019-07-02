14f0255d48ae6ea68eb4b57ce0c85dbdbfe93c77
Reid van Melle <rvanmelle@gmail.com>
2019-07-01 21:42:57 -0400

    updated package.swift file

--- Package.swift
+++ Package.swift
@@ -1,5 +1,27 @@
 import PackageDescription
 
 let package = Package(
-  name: "then"
+    name: "then",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "then",
+            targets: ["then"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "then",
+            dependencies: []),
+        .testTarget(
+            name: "thenTests",
+            dependencies: ["then"]),
+    ]
 )
+
